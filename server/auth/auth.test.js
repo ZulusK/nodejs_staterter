034c04b4fa9fe295bcaadd80a02f6d6d41848e3d
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-07-19 16:06:02 +0300

    feat(auth): Done auth.test

--- server/auth/auth.test.js
+++ server/auth/auth.test.js
@@ -35,11 +35,11 @@ describe('## Auth APIs', () => {
   describe('# POST /api/auth/signup', testSignup);
   describe('# POST /api/auth/confirm-phone', testConfirmPhone);
   describe('# POST /api/auth/login', testLogin);
-  //   describe('# GET /api/auth/token', testUpdateAccessToken);
-  //   describe('# POST /api/auth/check-access', testCheckAccessToken);
-  //   describe('# POST /api/auth/check-refresh', testCheckRefreshToken);
-  //   describe('# POST /api/auth/confirm-email', testConfirmEmail);
-  //   describe('# POST /api/auth/deactivate', testEmailDeactivate);
+    describe('# GET /api/auth/token', testUpdateAccessToken);
+    describe('# POST /api/auth/check-access', testCheckAccessToken);
+    describe('# POST /api/auth/check-refresh', testCheckRefreshToken);
+    describe('# POST /api/auth/confirm-email', testConfirmEmail);
+    describe('# POST /api/auth/deactivate', testEmailDeactivate);
 });
 
 function testSignup() {
@@ -302,3 +302,257 @@ function testLogin() {
       .catch(done);
   });
 }
+
+
+function testUpdateAccessToken() {
+    before((done) => {
+      clean(() => reqs.createAndLoginUser(userData).then((res) => {
+        tokens = res.tokens;
+        done();
+      }));
+    });
+    after(clean);
+  
+    describe('# Get /api/auth/token', () => {
+      it('should return JWT token', (done) => {
+        reqs
+          .makeUpdateAccessTokenReq(tokens.refresh.token)
+          .then((res) => {
+            expect(res.status).to.be.eq(httpStatus.OK);
+            tests.expectAccessJWTToken(res.body);
+            done();
+          })
+          .catch(done);
+      });
+      it('should not fail, verify new access token', (done) => {
+        reqs
+        .makeUpdateAccessTokenReq(tokens.refresh.token)
+        .then(res=>{
+            tests.expectAccessTokenIsValid(app, res.body.token, done)
+        })
+      });
+      it('should reject, used invalid token', (done) => {
+        reqs
+          .makeUpdateAccessTokenReq('bearer not-a-token-at-all')
+          .then((res) => {
+            expect(res.status).to.be.eq(httpStatus.UNAUTHORIZED);
+            done();
+          })
+          .catch(done);
+      });
+    });
+  }
+  
+  function testCheckAccessToken() {
+    const userData = {
+      mobileNumber: '+380500112836',
+      email: 'sample.mail@mail.com',
+      fullname: 'John Stark',
+      password: '123Asd@dmvc7',
+      creditCardNumber: '4111111111111111',
+      creditCardCVV: '456',
+      creditCardExpDate: '11/19'
+    };
+    let tokens = null;
+    before((done) => {
+      clean(() => reqs.createAndLoginUser(userData).then((res) => {
+        tokens = res.tokens;
+        done();
+      }));
+    });
+    beforeEach((done) => {
+      reqs
+        .makeLoginReq(userData)
+        .then((res) => {
+          tokens = res.body.tokens;
+          done();
+        })
+        .catch(done);
+    });
+    after(clean);
+    it('should not reject, used valid token', (done) => {
+      request(app)
+        .get('/api/auth/check-access')
+        .set('Authorization', `bearer ${tokens.access.token}`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, used invalid token', (done) => {
+      request(app)
+        .get('/api/auth/check-access')
+        .set('Authorization', 'bearer invalid.Token.here')
+        .expect(httpStatus.UNAUTHORIZED)
+        .then((res) => {
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, used outdated token', (done) => {
+      setTimeout(function () {
+        request(app)
+          .get('/api/auth/check-access')
+          .set('Authorization', `bearer ${tokens.access.token}`)
+          .expect(httpStatus.UNAUTHORIZED)
+          .then((res) => {
+            done();
+          })
+          .catch(done);
+      }, tokens.access.expiredIn * 1000 - Date.now());
+    });
+  }
+  
+  function testCheckRefreshToken() {
+    const userData = {
+      mobileNumber: '+380500112836',
+      email: 'sample.mail@mail.com',
+      fullname: 'John Stark',
+      password: '123Asd@dmvc7',
+      creditCardNumber: '4111111111111111',
+      creditCardCVV: '456',
+      creditCardExpDate: '11/19'
+    };
+    let tokens = null;
+    before((done) => {
+      clean(() => reqs.createAndLoginUser(userData).then((res) => {
+        tokens = res.tokens;
+        done();
+      }));
+    });
+    beforeEach((done) => {
+      reqs
+        .makeLoginReq(userData)
+        .then((res) => {
+          tokens = res.body.tokens;
+          done();
+        })
+        .catch(done);
+    });
+    after(clean);
+    it('should not reject, used valid token', (done) => {
+      request(app)
+        .get('/api/auth/check-refresh')
+        .set('Authorization', `bearer ${tokens.refresh.token}`)
+        .expect(httpStatus.OK)
+        .then((res) => {
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, used invalid token', (done) => {
+      request(app)
+        .get('/api/auth/check-refresh')
+        .set('Authorization', 'bearer invalid.Token.here')
+        .expect(httpStatus.UNAUTHORIZED)
+        .then((res) => {
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, used outdated token', (done) => {
+      setTimeout(function () {
+        request(app)
+          .get('/api/auth/check-refresh')
+          .set('Authorization', `bearer ${tokens.refresh.token}`)
+          .expect(httpStatus.UNAUTHORIZED)
+          .then((res) => {
+            done();
+          })
+          .catch(done);
+      }, tokens.refresh.expiredIn * 1000 - Date.now());
+    });
+  }
+  
+  function testConfirmEmail() {
+    const userData = {
+      mobileNumber: '+380500112836',
+      email: 'sample.mail@mail.com',
+      fullname: 'John Stark',
+      password: '123Asd@dmvc7',
+      creditCardNumber: '4111111111111111',
+      creditCardCVV: '456',
+      creditCardExpDate: '11/19'
+    };
+    let emailActivationToken = null;
+    before((done) => {
+      clean(() => reqs.createUser(userData).then((res) => {
+        emailActivationToken = res.body.emailActivationToken;
+        done();
+      }));
+    });
+    after(clean);
+    it('should return 200, email is not activated', (done) => {
+      reqs
+        .activateUserEmail(emailActivationToken)
+        .then(res => expect(res.status).to.be.eq(httpStatus.OK))
+        .then(() => done())
+        .catch(done);
+    });
+    it('should reject, email is activated', (done) => {
+      reqs
+        .activateUserEmail(emailActivationToken)
+        .then((res) => {
+          expect(res.status).to.be.eq(httpStatus.BAD_REQUEST);
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, used invalid token', (done) => {
+      request(app)
+        .post('/api/auth/confirm-email')
+        .set('Authorization', 'bearer invalid.token.here')
+        .expect(httpStatus.UNAUTHORIZED)
+        .then(() => done())
+        .catch(done);
+    });
+  }
+  
+  function testEmailDeactivate() {
+    const userData = {
+      mobileNumber: '+380500112836',
+      email: 'sample.mail@mail.com',
+      fullname: 'John Stark',
+      password: '123Asd@dmvc7',
+      creditCardNumber: '4111111111111111',
+      creditCardCVV: '456',
+      creditCardExpDate: '11/19'
+    };
+    let emailActivationToken = null;
+    before((done) => {
+      clean(() => reqs.createUser(userData).then((res) => {
+        emailActivationToken = res.body.emailActivationToken;
+        done();
+      }));
+    });
+    after(clean);
+    it('should return 200, email is not activated', (done) => {
+      reqs
+        .deactivateUserEmail(emailActivationToken)
+        .then((res) => {
+          expect(res.status).to.be.eq(httpStatus.OK);
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, email is activated', (done) => {
+      reqs
+        .deactivateUserEmail(emailActivationToken)
+        .then((res) => {
+          expect(res.status).to.be.eq(httpStatus.BAD_REQUEST);
+          done();
+        })
+        .catch(done);
+    });
+    it('should reject, use invalid token', (done) => {
+      reqs
+        .deactivateUserEmail("invalid-token")
+        .then((res) => {
+          expect(res.status).to.be.eq(httpStatus.UNAUTHORIZED);
+          done();
+        })
+        .catch(done);
+    });
+  }
+  
